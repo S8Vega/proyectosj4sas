203a27eca05da9da669e961c8a4ea6fabd4e9df0
S8Vega <bsebastianvg18@gmail.com>
2020-11-09 14:24:48 -0500

    commit inicial

---
+++ src/main/java/com/proyectosj4sas/app/Proyectosj4sasApplication.java
@@ -0,0 +1,13 @@
+package com.proyectosj4sas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectosj4sasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectosj4sasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectosj4sas/app/Proyectosj4sasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectosj4sas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectosj4sasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
