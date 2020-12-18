1a10aead8670eeacff7912642d1c3d77e4dcce27
Jeferson Hernandez <asimplemailmore@gmail.com>
2020-12-18 01:36:50 -0500

    modificacion de los input de las claves

--- src/main/java/com/proyectosj4sas/app/controlador/AfiliacionEpsControlador.java
+++ src/main/java/com/proyectosj4sas/app/controlador/AfiliacionEpsControlador.java
@@ -6,7 +6,6 @@ import com.proyectosj4sas.app.modelo.servicio.implementacion.EpsServicioImpl;
 import com.proyectosj4sas.app.modelo.servicio.implementacion.TrabajadorServicioImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
