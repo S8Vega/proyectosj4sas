0c2a9eda54a75f90f7d3ae100d1155090062753a
Jeferson Hernandez <asimplemailmore@gmail.com>
2020-12-18 06:26:54 -0500

    cambio de host

--- src/main/java/com/proyectosj4sas/app/modelo/servicio/implementacion/UsuarioServicioImpl.java
+++ src/main/java/com/proyectosj4sas/app/modelo/servicio/implementacion/UsuarioServicioImpl.java
@@ -133,7 +133,7 @@ public class UsuarioServicioImpl implements IServicio<Usuario, Long>, UserDetail
 
 			String content = TEXT_BODY.replace("$tokenValue", token);
 			content = content.replace("$username", usuario.getUsername());
-			content = content.replace("$host_to_deploy", "ec2-54-221-64-103.compute-1.amazonaws.com");
+			content = content.replace("$host_to_deploy", "ec2-3-208-12-106.compute-1.amazonaws.com");
 			// content=content.replace("$host_to_deploy", "localhost");
 			emailBody.setContent(content);
 			return emailPort.sendEmail(emailBody);
